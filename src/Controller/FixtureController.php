38ccf3eb404c627112b8710c27f0ba3bf4de5d89
Wolfgang Hinzmann <wolfgang.hinzmann@doccheck.com>
2025-01-29 22:32:36 +0100

    WIP

--- src/Controller/FixtureController.php
+++ src/Controller/FixtureController.php
@@ -2,30 +2,77 @@
 
 namespace App\Controller;
 
-use App\Content\File\Enum\ImageType;
-use App\Content\File\Form\Data\EditProfileImageData;
-use App\Content\File\Form\EditProfileImageType;
-use App\Content\File\ImageService;
-use App\Content\Team\TeamService;
+use App\Content\Fixture\ScoringMap;
 use App\Entity\Fixture;
-use App\Entity\Participant;
-use App\Entity\Team;
-use App\Repository\ParticipantRepository;
-use App\Repository\PlayerRepository;
-use App\Repository\TeamRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\Data\FixtureResultData;
+use App\Form\FixtureResultType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/match')]
-final class FixtureController extends AbstractController
+final class FixtureController extends BaseController
 {
     #[Route('/detail/{id}', name: 'match_detail')]
-    public function detail(Fixture $fixture): Response
+    public function detail(Fixture $fixture, Request $request): Response
     {
+//        $data = (new FixtureResultData());
+//        $form = $this->createForm(FixtureResultType::class, $data);
+//
+//        $form->handleRequest($request);
+
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            dd($form->getData());
+//            return $this->redirectToRoute('match_detail', ['id' => $fixture->getId()]);
+//        }
+
         return $this->render('match/detail.html.twig', [
+//            'data' => $data,
+//            'form' => $form,
             'fixture' => $fixture,
         ]);
+    }
+
+    #[Route('/edit/result/{id}', name: 'match_edit_result')]
+    public function editProfileImageAjax(Fixture $fixture, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $data = (new FixtureResultData());
+        $data->setHomeGoals($fixture->getHomeGoals());
+        $data->setAwayGoals($fixture->getAwayGoals());
+
+        $players = array_merge(
+            $fixture->getHomeParticipant()->getPlayers()->toArray(),
+            $fixture->getAwayParticipant()->getPlayers()->toArray()
+        );
+
+        $soringMap = new ScoringMap($players, $fixture->getScorers());
+        foreach ($soringMap->getMap() as $player => $scoreValue) {
+            $data->setPlayerGoal($player, $scoreValue);
+        }
+
+        $form = $this->createAjaxForm(FixtureResultType::class, $data,$request, ['players' => $players]);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var FixtureResultData $data */
+            $data = $form->getData();
+
+            $fixture->setHomeGoals($data->getHomeGoals());
+            $fixture->setAwayGoals($data->getAwayGoals());
+            $fixture->setPlayed(new \DateTime());
+            $fixture->setScorers($data->getPlayerGoals());
+
+            $entityManager->persist($fixture);
+            $entityManager->flush();
+
+            return $this->returnAjaxSuccessResponse(
+                'match_detail',
+                ['id' => $fixture->getId()],
+                $request
+            );
+        }
+
+        return $this->renderAjaxForm($form);
     }
 }
---
+++ src/Entity/BattleRound.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BattleRoundRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BattleRoundRepository::class)]
+class BattleRound
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\OneToOne(inversedBy: 'battleRound', cascade: ['persist', 'remove'])]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Tournament $tournament = null;
+
+    /**
+     * @var Collection<int, Participant>
+     */
+    #[ORM\ManyToMany(targetEntity: Participant::class, inversedBy: 'battleRounds')]
+    private Collection $participants;
+
+    /**
+     * @var Collection<int, Player>
+     */
+    #[ORM\ManyToMany(targetEntity: Player::class, inversedBy: 'battleRounds')]
+    private Collection $players;
+
+    public function __construct()
+    {
+        $this->participants = new ArrayCollection();
+        $this->players = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTournament(): ?Tournament
+    {
+        return $this->tournament;
+    }
+
+    public function setTournament(Tournament $tournament): static
+    {
+        $this->tournament = $tournament;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Participant>
+     */
+    public function getParticipants(): Collection
+    {
+        return $this->participants;
+    }
+
+    public function addParticipant(Participant $participant): static
+    {
+        if (!$this->participants->contains($participant)) {
+            $this->participants->add($participant);
+        }
+
+        return $this;
+    }
+
+    public function removeParticipant(Participant $participant): static
+    {
+        $this->participants->removeElement($participant);
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Player>
+     */
+    public function getPlayers(): Collection
+    {
+        return $this->players;
+    }
+
+    public function addPlayer(Player $player): static
+    {
+        if (!$this->players->contains($player)) {
+            $this->players->add($player);
+        }
+
+        return $this;
+    }
+
+    public function removePlayer(Player $player): static
+    {
+        $this->players->removeElement($player);
+
+        return $this;
+    }
+}
